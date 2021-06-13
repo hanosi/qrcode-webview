9b9d50bc8a284d60d8aeae10fd335fd7f87bf48d
hanosi <hanosiwazri94@gmail.com>
2021-06-13 22:06:53 +0800

    undanganFlutter

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# qrcode
+# undanganflutterproject
 
-A new Flutter application.
+A new Flutter project.
 
 ## Getting Started
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,15 @@
 import 'package:flutter/material.dart';
-import 'home_screen.dart';
+import 'landing.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MyApp());
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomeScreen(),
+      home: LandingPage(),
     );
   }
 }
